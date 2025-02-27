69fcc96a6ffb5d7bd0bf3473cae2d7cbac5f0b79
JD <jayprowings@gmail.com>
2025-02-27 09:13:58 +0530

    rolling file appender

--- src/main/java/com/prowings/MyLog4j2Example.java
+++ src/main/java/com/prowings/MyLog4j2Example.java
@@ -3,20 +3,40 @@ package com.prowings;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class MyLog4j2Example {
 	
-	private static final Logger logger = LogManager.getLogger(MyLog4j2Example.class);
+//	private static final Logger log = LogManager.getLogger(MyLog4j2Example.class);
 	
 	
 	public static void main(String[] args) {
 		
-        logger.info("This is an INFO message");
-        logger.warn("This is a WARN message");
-        logger.error("This is an ERROR message");
-        logger.fatal("This is a FATAL message");
-        logger.debug("This is a DEBUG message");
-        logger.trace("This is a TRACE message");
+		Integer x = 5;
+		Integer y = 10;
+		String message = "Hello, World!";
+		Student student = new Student("John", 25, "New York");
 		
+		
+		log.info("Received Student : {} is like this.", student);
+        log.info("This is an INFO message: {}, number1:{} and number2:{}", message, x, y);
+        log.warn("This is a WARN message");
+        log.error("This is an ERROR message");
+        
+        log.fatal("This is a FATAL message");
+        log.debug("This is a DEBUG message");
+        log.trace("This is a TRACE message");		
+
+        while (true) {
+            log.info("This is a test log message at: " + System.currentTimeMillis());
+            try {
+                Thread.sleep(1000);  // Log every 1 second
+            } catch (InterruptedException e) {
+                log.error("Thread interrupted", e);
+            }
+        }
+        
 	}
 	
 }
